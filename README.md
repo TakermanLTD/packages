523361466a762586398a56d837f0745f38dc0a03
Tanyo Ivanov <tivanov@takerman.net>
2023-09-28 09:21:11 +0300

    Dispose connection and clientafter send

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 * Packages
 
-nuget push Takerman.Mail.1.0.0.nupkg -ApiKey [apikey] -Source https://pkgs.dev.azure.com/Takerman/_packaging/Takerman/nuget/v3/index.json
+nuget push Takerman.Mail.1.0.1.nupkg -ApiKey [apiKey] -Source https://pkgs.dev.azure.com/Takerman/_packaging/Takerman/nuget/v3/index.json
--- Takerman.Mail/MailService.cs
+++ Takerman.Mail/MailService.cs
@@ -1,32 +1,58 @@
 ﻿using Newtonsoft.Json;
 using RabbitMQ.Client;
+using System;
 using System.Text;
 
 namespace Takerman.Mail
 {
     public class MailService : IMailService
     {
+        private RabbitMqConfig _rabbitMqConfig;
+
         public void SendToQueue(MailMessageDto mailMessage, RabbitMqConfig rabbitMqConfig)
         {
+            _rabbitMqConfig = rabbitMqConfig;
             var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(mailMessage));
 
-            var rabbitMq = new ConnectionFactory()
+            var connectionFactory = new ConnectionFactory()
             {
                 HostName = rabbitMqConfig.Hostname,
                 UserName = rabbitMqConfig.Username,
                 Password = rabbitMqConfig.Password,
                 Port = rabbitMqConfig.Port,
                 DispatchConsumersAsync = true
-            }.CreateConnection();
+            };
 
-            var channel = rabbitMq.CreateModel();
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    try
+                    {
+                        channel.QueueDeclare(_rabbitMqConfig.Queue, durable: false, exclusive: false, autoDelete: false);
+                        channel.ExchangeDeclare(_rabbitMqConfig.Exchange, ExchangeType.Direct, durable: false, autoDelete: false);
 
-            channel.BasicPublish(
-                exchange: rabbitMqConfig.Exchange,
-                routingKey: rabbitMqConfig.RoutingKey,
-                mandatory: false,
-                basicProperties: null,
-                body: body);
+                        channel.BasicPublish(
+                            exchange: rabbitMqConfig.Exchange,
+                            routingKey: rabbitMqConfig.RoutingKey,
+                            mandatory: false,
+                            basicProperties: null,
+                            body: body);
+                    }
+                    catch (Exception ex)
+                    {
+                        var message = ex.Message + (ex.InnerException == null ? string.Empty : ex.InnerException.Message);
+                        Console.WriteLine($"Exception while publishing a message in the package: {message}");
+                    }
+                    finally
+                    {
+                        channel.Close();
+                        connection.Close();
+                        channel.Dispose();
+                        connection.Dispose();
+                    }
+                }
+            }
         }
     }
 }

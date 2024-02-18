55fcd8f02de42f2c0a22803a0f0e1fddc505349b
Tanyo Ivanov <tivanov@takerman.net>
2024-02-18 23:45:55 +0200

    new version

--- Takerman.Notifications/MailMessageDto.cs
+++ Takerman.Notifications/MailMessageDto.cs
@@ -2,11 +2,13 @@
 {
     public class MailMessageDto
     {
+        public string Name { get; set; } = "User";
+
         public string From { get; set; }
 
-        public string To { get; set; }
+        public string To { get; set; } = "tivanov@takerman.net";
 
-        public string Subject { get; set; }
+        public string Subject { get; set; } = "New message from App";
 
         public string Body { get; set; }
     }

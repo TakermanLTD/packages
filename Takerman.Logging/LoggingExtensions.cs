b70ac4705545de1e2bce8147774641ec8de7f5b0
Tanyo Ivanov <tivanov@takerman.net>
2024-10-20 15:14:39 +0300

    logging

---
+++ Takerman.Logging/LoggingExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Slack;
+
+namespace Takerman.Logging
+{
+    public static class LoggingExtensions
+    {
+        public static Serilog.ILogger TakermanLogger => new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Warning)
+                .WriteTo.Slack("https://hooks.slack.com/services/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", restrictedToMinimumLevel: LogEventLevel.Error)
+                .CreateLogger();
+
+        public static Serilog.ILogger AddTakermanLogging(this ILoggingBuilder builder)
+        {
+            builder.AddSerilog(TakermanLogger);
+
+            return TakermanLogger;
+        }
+
+        public static Serilog.ILogger AddTakermanLogging(this IHostBuilder host)
+        {
+            host.UseSerilog(TakermanLogger);
+
+            return TakermanLogger;
+        }
+    }
+}
